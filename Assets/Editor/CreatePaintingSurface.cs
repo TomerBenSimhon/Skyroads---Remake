177a09bfe306f1904e82b4fd9b79b2e9afd222b3
ShmueLapanim <tomeriko0011@gmail.com>
2025-08-13 21:10:33 +0300

    added erase tool

--- Assets/Editor/CreatePaintingSurface.cs
+++ Assets/Editor/CreatePaintingSurface.cs
@@ -13,5 +13,22 @@ public class CreatePaintingSurface : EditorWindow
         plane.layer = LayerMask.NameToLayer("Painting Surface");
         plane.GetComponent<MeshRenderer>().enabled = false;
     }
+    
+    public static GameObject CreatePaintingSurfaceAtPosition(Vector3 position)
+    {
+        GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        plane.name = "PaintingSurface_Temp";
+        plane.transform.position = position;
+        plane.transform.localScale = Vector3.one * 1000f;
+        plane.layer = LayerMask.NameToLayer("Painting Surface");
+
+        // invisible + not saved
+        var renderer = plane.GetComponent<MeshRenderer>();
+        if (renderer)
+            renderer.enabled = false;
+
+        plane.hideFlags = HideFlags.DontSave;
+        return plane;
+    }
 
 }
